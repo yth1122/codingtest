9ad10c108a990da5567944855d1eda398177cc4d
yth1122 <74342126+yth1122@users.noreply.github.com>
2024-05-16 16:07:51 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 82.17 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,17 +2,16 @@ function solution(elements) {
     //최초 1개씩 set에 추가    
     let set = new Set(elements);
     // cursor 배열의 index, check elements      
-	let cursor = 0,check = 0,num=elements[0];
+	let cursor = 1,check = 0,num=elements[0];
     //배열의 전체를 더해서 추가     
     set.add(elements.reduce((a,b) => a+b ,0))
 	while(elements.length != check){
-		cursor+=1;
 		num += elements[cursor];
 		set.add(num);
-		
-		if(cursor == elements.length-2){
+		cursor+=1;
+		if(cursor == elements.length-1){
 			
-			cursor = 0;
+			cursor = 1;
 			elements.push(elements.shift());
 		 	num = elements[0];
 			check+=1;
