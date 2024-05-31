22e1affca7c4934fe168c407b5aea66a4cbbe808
yth1122 <74342126+yth1122@users.noreply.github.com>
2024-05-31 09:28:06 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -0,0 +1,26 @@
+function solution(arrayA, arrayB) {
+    	let arr = [];
+		let getGCD = (num1, num2) => (num2 > 0 ? getGCD(num2, num1 % num2) : num1);
+		let gcdA; 
+		let gcdB;
+        for(let i=0; i<arrayA.length; i++){
+            gcdA = getGCD(gcdA,arrayA[i]);
+            gcdB = getGCD(gcdB,arrayB[i]) 
+        }
+		let checkA = arrayA.every(value=> value % gcdA == 0);
+		let checkB = arrayB.every(value=> value % gcdB == 0);
+		
+		if(checkA && gcdA!=1){ 
+           if(!arrayB.some(value=> value % gcdA ==0 ))  arr.push(gcdA)
+        };
+    
+		if(checkB && gcdB!=1 ){ 
+            if(!arrayA.some(value=> value % gcdB ==0 ))  arr.push(gcdB)
+        };
+	    
+    
+    
+    return arr.length?  arr.sort((a,b)=>b-a)[0] :0 ;
+}
+
+
