b3c3bf90e150e1e518f7668b84b228f4816215de
yth1122 <yxk1212@naver.com>
2024-06-10 19:45:20 +0900

    2024-06-10 유사칸토어비트열 1차 fail 효율성 고려하여 다시 풀예정

--- test/codingtest.js
+++ test/codingtest.js
@@ -1,13 +1,21 @@
+	// //https://school.programmers.co.kr/learn/courses/30/lessons/148652 시간초과 66.7점
 	function test() {
-		console.log(solution(2554));
+		console.log(solution(3,4,17));
 	}
 	
 
-	function solution(storey) {
-		let string_storey = (''+storey);
-		console.log(string_storey[0],string_storey.length);
-		// let compare = '1'.padEnd(leng,'0');
-		
+	function solution(n,l,r) {
+		let start = '1';
+		let result = 0;
+		while(n>0){
+			n-=1;
+			start = start.split('0').join('00000').split('1').join('11011');
+		}
+		for(let i =l-1; i<r;i++ ){
+			console.log(start[i]);
+			result+= +start[i];
+		}
+		console.log(start);
 	}
 
 module.exports = test;
