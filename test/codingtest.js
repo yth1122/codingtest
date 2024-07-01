68d69843e63b13d3248953e28b5d55252e9b1332
yth1122 <yxk1212@naver.com>
2024-07-01 19:00:29 +0900

    백준 2178 2024-07-01

--- test/codingtest.js
+++ test/codingtest.js
@@ -5,6 +5,15 @@
 
 // https://www.acmicpc.net/problem/2178
 	function solution(n,l,r) {
+		// 오르막수 11057
+		// 0,1,2,3,4,5,6,7,8,9
+		let input =3 ;
+		let dp = Array.from({length:1000}).fill(0);
+		dp[1] = 10;
+		
+
+
+
 	}
 
 module.exports = test;
