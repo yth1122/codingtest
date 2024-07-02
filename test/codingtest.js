8127489d6ec4176f3c51b10d39fd4463d63daeb4
yth1122 <yxk1212@naver.com>
2024-07-02 19:31:20 +0900

    백준 dp notsolve 2024-07-02

--- test/codingtest.js
+++ test/codingtest.js
@@ -6,12 +6,13 @@
 // https://www.acmicpc.net/problem/2178
 	function solution(n,l,r) {
 		// 오르막수 11057
-		// 0,1,2,3,4,5,6,7,8,9
+
+		// 0,1,2,3,4,5,6,7,8,9 = 10 , 1 9 ,2 8, 3 7, 4 6  5 5
+		// 10 , 55  10*11 / 2 ,  자리수 마다 늘어남  
 		let input =3 ;
 		let dp = Array.from({length:1000}).fill(0);
 		dp[1] = 10;
-		
-
+		// 
 
 
 	}
