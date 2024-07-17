b32416f8b130ce7b97728bfcf27135761f4d02a2
yth1122 <yxk1212@naver.com>
2024-07-17 16:30:24 +0900

    2024-07-17

--- test/codingtest.js
+++ test/codingtest.js
@@ -5,6 +5,37 @@
 
 // https://www.acmicpc.net/problem/2178
 	function solution(n,l,r) {
+		let input = 
+`dmih
+11
+B
+B
+P x
+L
+B
+B
+B
+P y
+D
+D
+P z`;
+		input = input.split('\n');
+		let [ text, size ] = input.splice(0,2);
+		let index = text.length-1;
+		console.log(text,input);
+		for(let action of input){
+			console.log(action,text[index],index);
+			if(action == 'L'){
+				index = index > 0 ? index-1 : index ;
+			}else if(action == 'D'){
+				
+			}else if(action == 'B'){
+				index = text.length -1 > index ? index +1 :index;
+			}else {
+				console.log(text.substr(0,index),text.substr(index,text.length-1 -index))
+			}
+		}
 	}
+
 
 module.exports = test;
