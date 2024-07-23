62364dea4d9f8c88437a0a2ef7b9f605d7ac9524
yth1122 <yxk1212@naver.com>
2024-07-23 16:24:24 +0900

    2024-07-23

--- test/codingtest.js
+++ test/codingtest.js
@@ -1,10 +1,13 @@
-	// //https://school.programmers.co.kr/learn/courses/30/lessons/148652 시간초과 66.7점
+	// 
 	function test() {
 		console.log(solution(3,4,17));
 	}
 
-// https://www.acmicpc.net/problem/2178
+// https://www.acmicpc.net/problem/2078
 	function solution(n,l,r) {
+		let start= [1,1];
+		let input = `3 4`.split(' ');
+		console.log(input)
 	}
 
 
