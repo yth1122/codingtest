63719fa83a1194fd8ec7fecf14b9e95162d6f5a3
yth1122 <yxk1212@naver.com>
2024-07-24 16:14:33 +0900

    2024-07-24

--- test/codingtest.js
+++ test/codingtest.js
@@ -5,9 +5,10 @@
 
 // https://www.acmicpc.net/problem/2078
 	function solution(n,l,r) {
-		let start= [1,1];
-		let input = `3 4`.split(' ');
-		console.log(input)
+		let input = `10 3`.split(' ').map(Number);
+		let result = input[0] < input[1] ? `${input[0]-1} ${(input[1]-1)/input[0]}` : `${(input[0]-1)/input[1]} ${input[1]-1}`;
+		console.log(result);
+		
 	}
 
 
