f65faa1df6b0556d58bd225fd2f88ab1bf3bcfde
yth1122 <yxk1212@naver.com>
2024-07-26 16:30:10 +0900

    2024-07-26

--- test/codingtest.js
+++ test/codingtest.js
@@ -6,8 +6,6 @@
 // https://www.acmicpc.net/problem/2078
 	function solution(n,l,r) {
 		let input = `3 4`.split(' ').map(Number);
-		let result = input[0] < input[1] ? `${input[0]-1} ${(input[1]-1)/input[0]}` : `${(input[0]-1)/input[1]} ${input[1]-1}`;
-		console.log(result);
 		
 	}
 
