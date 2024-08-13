3c78ad0a2e527b2f91b552f04dcc308b8e3f0b57
yth1122 <yxk1212@naver.com>
2024-08-13 16:14:47 +0900

    2024-08-13

--- test/codingtest.js
+++ test/codingtest.js
@@ -6,29 +6,45 @@
 	function solution(n,l,r) {
 		let input = 
 `(()[[]])([])`;
-		let stack = [];
-		let result = 0;
-		for(let i = 0; i<input.length; i++){
-			if(input[i] === ")"){
-				if(stack[stack.length-1] === "("){
-					result += 2 ;
-					stack.pop();
-				} else {
-					stack.push(input[i]);
-				}
-			}else if (input[i] === "]"){
-				if(stack[stack.length-1] === "["){
-					result +=3;
-					stack.pop();
-				} else {
-					stack.push(input[i]);
-				}
-			}else {
-				stack.push(input[i]);
-			}
-		}
-		console.log(stack,result);
-	}
+const bracketMap = {
+	'()': 2,
+	'[]': 3,
+};
+
+function getBracketValue(str, map) {
+	let stack = [];
+	let value = '';
+	console.log(str);
+	for (let i of str) {
+	// 		if (!map[i]) {
+	// 				if (i === ')') stack.push(')');
+	// 				else if (i === ']') stack.push(']');
+	// 		} else {
+	// 				value += i;
+	// 		}
+	// 		if ((stack[0] === ')' && i === '(') || (stack[0] === ']' && i === '[')) {
+	// 				stack.shift();
+	// 				return getBracketValue(value, map);
+	// 		}
+	// }
+	// let result = 0;
+	// while(stack.length > 0) {
+	// 			const top = stack.pop();
+	// 			if(top === ')') {
+	// 					result += bracketMap['()'] * Number(getBracketValue(value, map));
+	// 					value = '';
+	// 			} else if(top === ']'){
+	// 					result += bracketMap['[]']*Number(getBracketValue(value, map));
+	// 					value = '';
+	// 			}
+	// 	}
+	// 	return result + Number(getBracketValue(value, map));
+}
+
+	// const arr = input.split('');
+	const result = getBracketValue(input, bracketMap);
+		
+}
 
 
 module.exports = test;
