25f553bb0871edc4d0228805007ac207460e43ba
yth1122 <yxk1212@naver.com>
2024-08-14 16:26:28 +0900

    2024-08-14

--- test/codingtest.js
+++ test/codingtest.js
@@ -4,47 +4,10 @@
 	}
 
 	function solution(n,l,r) {
-		let input = 
-`(()[[]])([])`;
-const bracketMap = {
-	'()': 2,
-	'[]': 3,
-};
 
-function getBracketValue(str, map) {
-	let stack = [];
-	let value = '';
-	console.log(str);
-	for (let i of str) {
-	// 		if (!map[i]) {
-	// 				if (i === ')') stack.push(')');
-	// 				else if (i === ']') stack.push(']');
-	// 		} else {
-	// 				value += i;
-	// 		}
-	// 		if ((stack[0] === ')' && i === '(') || (stack[0] === ']' && i === '[')) {
-	// 				stack.shift();
-	// 				return getBracketValue(value, map);
-	// 		}
-	// }
-	// let result = 0;
-	// while(stack.length > 0) {
-	// 			const top = stack.pop();
-	// 			if(top === ')') {
-	// 					result += bracketMap['()'] * Number(getBracketValue(value, map));
-	// 					value = '';
-	// 			} else if(top === ']'){
-	// 					result += bracketMap['[]']*Number(getBracketValue(value, map));
-	// 					value = '';
-	// 			}
-	// 	}
-	// 	return result + Number(getBracketValue(value, map));
-}
 
-	// const arr = input.split('');
-	const result = getBracketValue(input, bracketMap);
 		
-}
+	}
 
 
 module.exports = test;
