4e8faec2dc830aeaa56c81066e06a0154a9876bd
yth1122 <yxk1212@naver.com>
2024-08-19 16:20:25 +0900

    2024-08-19

--- test/codingtest.js
+++ test/codingtest.js
@@ -4,9 +4,17 @@
 	}
 
 	function solution(n,l,r) {
+		let input = '(()[[]])([])'.split('');
+		console.log(input);
+		let stack = [];
+		for(let i=0; i < input; i++){
+			if(key == '(' || key == '['){
+				stack.push(input[i]);
+			}else{
 
-
-		
+			}
+		}
+		console.log(stack);
 	}
 
 
