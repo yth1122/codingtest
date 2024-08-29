2225bc78dbd8ccce1f4a07a9f9bb3716a6833c3c
yth1122 <yxk1212@naver.com>
2024-08-29 16:17:44 +0900

    2024-08-29

--- test/codingtest.js
+++ test/codingtest.js
@@ -6,21 +6,55 @@
 	function solution() {
 		let input = '(()[[]])([])'.split('');
 		let stack = [];
-		for(let i=0; i < input.length; i++){
-			if(input[i] == '(' || input[i] == '['){
-				stack.push(input[i]);
-			}else if(input[i]==')' || input[i]==']'){
-				let check = stack[stack.length-1];
-				if(check == ']'){
 
-				}else if(check == ')'){
-
+		if(available(input)){
+			for(let i=0; i < input.length; i++){
+				if(input[i] == '(' || input[i] == '['){
+					stack.push(input[i]);
+				}else if(input[i]==')' || input[i]==']'){
+					let check = stack[stack.length-1];
+					if(check == '[' && input[i] == ']'){
+						stack.pop();
+						stack.push(3);
+					}else if(check == '(' && input[i] == ')'){
+						stack.pop();
+						stack.push(2);
+					}else{
+						let num = 0;
+						
+						while(1){
+							let last = stack.pop();
+							// console.log('ji');
+							break;
+						}
+					}
+	
 				}
-
+				// console.log(stack);
 			}
+		}else{
+			console.log(0);
 		}
-		console.log(stack);
 	}
+		
+	
+	function available(input){
+			let check = [];
+			for(let i=0; i<input.length; i++){
+				let prev= check[check.length-1];
+				if(input[i]==')' && prev == '(' ){
+					check.pop();
+				}else if(input[i]==']' && prev == '['){
+					check.pop();
+				}else{
+					check.push(input[i]);
+				}
+				console.log(check);
+			}
+			return check.length ? false: true;	
+	}
+		
+	
 
 
 module.exports = test;
