0165f597eb99931335623a65002a251c842abeeb
taeng <74342126+yth1122@users.noreply.github.com>
2024-09-19 09:42:26 +0900

    [Gold V] Title: AC, Time: 1636 ms, Memory: 21784 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+let input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+let num = input.shift();
+let controlArray = ( array, reverse ) =>{
+	if(reverse){
+		array.pop();
+	}else if(!reverse){
+		array.shift();
+	}
+	return array;
+}
+
+while(input.length){
+	let [command , size , stringArray] = input.splice(0,3);
+	let array = JSON.parse(stringArray);
+	let reverse = false;
+    let error = false;
+	for(let i=0; i<command.length; i++){
+		if(command[i] == 'R' ){
+			reverse = !reverse;
+		}else if(command[i] == 'D'){
+			if(array.length == 0){
+				error = true;
+			}else{
+				array = controlArray(array,reverse);
+			}	
+		}
+	}
+	console.log( !error ? JSON.stringify(reverse ? array.reverse(): array) : 'error');
+};
