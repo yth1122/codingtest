215c54dcde46323b60e07a0ea08a60a728e3c79a
yth1122 <yxk1212@naver.com>
2024-09-20 10:58:37 +0900

    2024-09-20

--- test/codingtest.js
+++ test/codingtest.js
@@ -5,12 +5,28 @@
 
 	function solution() {
 		let input =
-`4 6
-a t c i s w x`.split('\n');
-		
+`3 7 7`.split(' ').map(Number);
+		let [size , X, Y] = input;
+		Z(size);
+		let arr = [];
+		function Z(size,array){
+			let arr = [];
+			if(size >= 1 ){
+				for(let i=0; i<size; i++ ){
+					let count = -1;
+					let innerArray = [];
+					for(let j=0; j<4; j++){
+						count ++;
+						innerArray.push(count);
+					}
+					console.log(innerArray);
+				}
+			}
+		}
 	}	
 		
 	
-
-
+// [[[0,1,2,3][4,5,6,7][8,9,10,11][12,13,14,15]][[][][][]][[][][][]][[][][][]]]
+// [[0,1,2,3][4,5,6,7][8,9,10,11][12,13,14,15]]
+// [[0,1,2,3]]
 module.exports = test;
