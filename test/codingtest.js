0c9646ea210b298f79e9da46fa69b2e04c069d72
yth1122 <yxk1212@naver.com>
2024-09-23 16:22:42 +0900

    2024-09-23

--- test/codingtest.js
+++ test/codingtest.js
@@ -7,24 +7,14 @@
 		let input =
 `3 7 7`.split(' ').map(Number);
 		let [size , X, Y] = input;
-		Z(size);
+		Z(X,Y,Math.pow(size,2));
 		let arr = [];
-		function Z(size,array){
-			let arr = [];
-			if(size >= 1 ){
-				for(let i=0; i<size; i++ ){
-					let count = -1;
-					let innerArray = [];
-					for(let j=0; j<4; j++){
-						count ++;
-						innerArray.push(count);
-					}
-					console.log(innerArray);
-				}
-			}
+		function Z(X,Y,size){
+			console.log(X,Y,size);
 		}
+
 	}	
-		
+
 	
 // [[[0,1,2,3][4,5,6,7][8,9,10,11][12,13,14,15]][[][][][]][[][][][]][[][][][]]]
 // [[0,1,2,3][4,5,6,7][8,9,10,11][12,13,14,15]]
