1e7002638e757a7490f4441867405636d79198d3
yth1122 <yxk1212@naver.com>
2024-09-30 16:00:26 +0900

    2024-09-30

--- test/codingtest.js
+++ test/codingtest.js
@@ -7,14 +7,27 @@
 		let input =
 `3 7 7`.split(' ').map(Number);
 		let [size , X, Y] = input;
-		Z(0,0,Math.pow(size,2));
+		
 		let res = 0;
-		function Z(curx,cury,size){
-			if(curx == X && cury == Y  ){
 
+		Z(0,0,Math.pow(2,size));
+
+		function Z(curx,cury,cursize){
+			console.log(curx,cury,cursize);
+			if(curx == X && cury == Y  ){
+				console.log(res);
 			};
 			
-			console.log(X,Y,size);
+			if(curx <= X &&  X < curx + cursize && cury <= Y && Y < cury+cursize ){
+				cursize = parseInt(cursize/2);
+				Z(curx, cury, cursize);
+				Z(curx, cury + cursize, cursize);
+				Z(curx + size, cury, cursize);
+				Z(curx + size, cury + cursize, cursize);
+			}else {
+				res += cursize * cursize
+			}
+			
 		}
 
 	}	
