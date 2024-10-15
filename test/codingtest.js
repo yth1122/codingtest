4463a238f943b607f205ee6cc22c7aab02d47814
yth1122 <yxk1212@naver.com>
2024-10-15 18:53:36 +0900

    2024-10-15

--- test/codingtest.js
+++ test/codingtest.js
@@ -4,11 +4,15 @@
 	}
 
 	function solution() {
-		let input ='';
-
-
+		let input =['a','b','c'];
+		let input2 = 'stt';
+		let input3 = new String('stt')
+		// trans(input2);
+		console.log(input2 === input3,input3);
 	}	
 
-	
+	function trans(arr){
+		arr ='good'
+	}
 
 module.exports = test;
