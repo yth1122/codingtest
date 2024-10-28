da3c7867c1dea90df6c48433b87835020f201b9e
yth1122 <yxk1212@naver.com>
2024-10-28 18:44:42 +0900

    2024-10-28

--- test/codingtest.js
+++ test/codingtest.js
@@ -4,8 +4,13 @@
 	}
 
 	function solution() {
-		let input = '';
+		let input =
+`5
+6 9 5 7 4`.split('\n');
+		let count = input.shift();
+		input = input[0].split(' ').map(Number);	
 		console.log(input);
+		let arr = Array.from({length:count}).fill(0);	
 	}	
 
 
