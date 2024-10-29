0452e66409671dc4bff3bee1382f035d3a14ec1c
yth1122 <yxk1212@naver.com>
2024-10-29 18:55:22 +0900

    2024-10-29

--- test/codingtest.js
+++ test/codingtest.js
@@ -9,8 +9,8 @@
 6 9 5 7 4`.split('\n');
 		let count = input.shift();
 		input = input[0].split(' ').map(Number);	
-		console.log(input);
 		let arr = Array.from({length:count}).fill(0);	
+	
 	}	
 
 
