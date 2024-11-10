8d51409c228d0ca096e0a7ca5b0217b3261c1a64
yth1122 <yxk1212@naver.com>
2024-11-11 07:59:18 +0900

    2024-11-11

--- test/codingtest.js
+++ test/codingtest.js
@@ -8,14 +8,21 @@
 `6 4`;
 		console.log(input);
 		let [size,count] = input.split(' ').map(Number);
-		console.log(size,count);
 		// 0 ~ size count로 묶어서 덧셈
 		let dp = Array.from({length:size+1},()=> Array.from({length:count+1}).fill(0));
 		
 		for (let j = 1; j <= count; j++) {
 			dp[0][j] = 1; 
 		}
+		for (let i = 1; i <= size; i++) {
+			for (let j = 1; j <= count; j++) {
+					dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1000000000;
+			}
+		}
+		return dp[size][count];
+		
 	}	
 
 
 module.exports = test;
+
