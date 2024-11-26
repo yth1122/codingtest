374c9671e90d6ba5e465e456333c8845b3215e02
yth1122 <yxk1212@naver.com>
2024-11-26 18:29:15 +0900

    2024-1126

--- test/codingtest.js
+++ test/codingtest.js
@@ -2,9 +2,11 @@
 	function test() {
 		console.log(solution());
 	}
-
+	// 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 0초 후에 2*X의 위치로 이동하게 된다
+	// 더하기 or 빼기 갯수찾기
 	function solution() {
-		let input ='';
+		let [start,end] ='5 17'.split(' ').map(Number);
+		console.log(start, end);
 	}	
 
 
