a33b6656786a1e805ffd0ebdbe1f38c3e629124c
yth1122 <yxk1212@naver.com>
2024-12-17 16:00:36 +0900

    2024-12-17

--- test/codingtest.js
+++ test/codingtest.js
@@ -2,10 +2,10 @@
 	function test() {
 		console.log(solution());
 	}
-	// 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 0초 후에 2*X의 위치로 이동하게 된다
-	// 더하기 or 빼기 갯수찾기
 	function solution() {
-		
+		let input = `500000
+		8
+		0 2 3 4 6 7 8 9`;
 	}	
 
 
