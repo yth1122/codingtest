0e9f9604ab760f0ee11259c1543f745b368b55cc
yth1122 <yxk1212@naver.com>
2024-12-23 15:59:44 +0900

    2024-12-23

--- test/codingtest.js
+++ test/codingtest.js
@@ -3,9 +3,13 @@
 		console.log(solution());
 	}
 	function solution() {
-		let input = `500000
-		8
-		0 2 3 4 6 7 8 9`;
+		// 1107 리모컨
+		let [goal , count , brokenArray] = `500000
+8
+0 2 3 4 6 7 8 9`.split('\n');
+		brokenArray.split(' ').map(Number);
+		console.log(goal,brokenArray);
+
 	}	
 
 
